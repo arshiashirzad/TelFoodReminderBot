525deea6deeb01c487ccdc93e752be242202dea1
Arshia <154830026+arshiashirzad@users.noreply.github.com>
2025-05-18 15:41:17 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -82,7 +82,7 @@ UNIVERSITY_CONFIG = {
     "تهران": {
         "day_of_week": "sun",
         "hour": 15,
-        "minute": 35,
+        "minute": 45,
         "reminder_message": "⏰ یادآوری: ۲۴ ساعت تا پایان مهلت رزرو غذای دانشگاه تهران باقی مانده!"
     },
 }
